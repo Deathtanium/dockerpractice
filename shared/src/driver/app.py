560158637e76fe2ca397e6fc5043639e61ec0857
Deathtanium <mishouro08@gmail.com>
2023-11-24 22:38:12 +0200

    first proper com

--- shared/src/driver/app.py
+++ shared/src/driver/app.py
@@ -2,6 +2,10 @@ import socket
 import logging as log
 import os
 import json
+import time
+import watchdog as wd
+
+from watchdog.observers import Observer
 
 input_folder = "/shared/io/step1"
 #output_folder = "/shared/step2"
@@ -13,29 +17,52 @@ rhost = 'unpacker'
 rport = 5002
 
 log.info("Starting driver")
-while True:
-    for filename in os.listdir(input_folder):
-        log.info("Processing file: {}".format(filename))
+
+def read_till_null(sock:socket.socket):
+    data = b''
+    while True:
+        d = sock.recv(1024)
+        if not d:
+            break
+        data += d
+        if b'\0' in d:
+            break
+    data = data[:-1]
+    return data
+
+def sendfile(path):
+    if os.path.isfile(path):
+        log.info("sendfile - processing file: {}".format(os.path.basename(path)))
         try:
-            with open(os.path.join(input_folder, filename), "rb") as f:
-                data = f.read()
-            log.info("Sending request to unpacker")
+            with open(path, "rb") as f:
+                data = f.read().decode('latin-1')
+            log.info("sendfile - sending request to unpacker")
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((rhost, rport))
             data = {
-                    'filename':filename,
+                    'filename':os.path.basename(path),
                     'bytes': data
                 }
             s.send(json.dumps(data).encode())
-            r = s.recv(1024)
+            s.send(b'\0')
+            r = read_till_null(s)
             s.close()
             r = json.loads(r.decode())
-            log.info("Received response from unpacker")
+            log.info("sendfile - received response from unpacker")
             if r['status'] != 'ok':
-                log.error("Error processing file: {}".format(r.text))
-                continue
-            os.remove(os.path.join(input_folder, filename))
+                log.error("sendfile - error processing file; check unpacker logs")
+                return
+            os.remove(path)
         except Exception as e:
-            log.error("Error processing file: {}".format(e))
-            continue
-        
+            log.error("sendfile - error processing file: {}".format(e))
+            return
+
+found_files = True
+while True:
+    time.sleep(5)
+    if found_files:
+        log.info("main - checking for new files")
+        found_files = False
+    for f in os.listdir(input_folder):
+        found_files = True
+        sendfile(os.path.join(input_folder, f))
--- shared/src/unpacker/app.py
+++ shared/src/unpacker/app.py
@@ -16,10 +16,13 @@ shutil.register_unpack_format('7zip', ['.7z'], py7zr.unpack_7zarchive)
 lhost = 'driver'
 lport = 5001
 
+self_host = 'unpacker'
+self_port = 5002
+
 rhost = 'optimizer'
 rport = 5003
 
-tmp_folder = '/shared/io/step2'
+out_folder = '/shared/io/step2'
 
 supported_archive = ['7z','zip','rar']
 supported_image = ['png']
@@ -28,7 +31,10 @@ def handle_data(data:bytes):
     log.info('handle_data - processing data') 
     try:
         data = json.loads(data.decode())
-        filename, filebytes = data['filename'], data['bytes']
+        filename = data['filename']
+        filebytes:bytes = data['bytes'].encode('latin-1')
+        with open(os.path.join(out_folder, filename), 'wb') as f:
+            f.write(filebytes)
         ext = filename.split('.')[-1]
         if ext == "png":
             #send to optimizer
@@ -40,28 +46,37 @@ def handle_data(data:bytes):
             ret = json.dumps(ret).encode()
             return ret
         else:
-            log.error("Unsupported file type: {}".format(ext))
+            log.warn("Unsupported file type: {}".format(ext))
             return b''
     except Exception as e:
         log.error('handle_data - {}'.format(e))
         return b''
     
+def read_till_null(sock:socket.socket):
+    data = b''
+    while True:
+        d = sock.recv(1024)
+        if not d:
+            break
+        data += d
+        if b'\0' in d:
+            break
+    data = data[:-1]
+    return data
+    
 def thread_handler(client:socket.socket, addr):
     log.info('thread_handler - new connection from {}'.format(addr))
     while True:
         try:
-            data = client.recv(1024)
+            data = read_till_null(client)
             if data:
                 fwd_data = handle_data(data)
                 res = b''
-                if fwd_data:
-                    res = json.dumps({'status': 'ok'}).encode()
-                else:
-                    res = json.dumps({'status': 'error'}).encode()
+                res = json.dumps({'status': 'ok'}).encode() + b'\0'
                 client.send(res)
                 rsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 rsock.connect((rhost, rport))
-                rsock.send(fwd_data)
+                rsock.send(fwd_data + b'\0')
                 rsock.close()
             else:
                 log.info('thread_handler - connection closed by {}'.format(addr))
@@ -75,10 +90,10 @@ def thread_handler(client:socket.socket, addr):
 
 log.basicConfig(level=log.INFO, handlers=[fh])
 log.info('main - starting unpacker')
-if not os.path.exists(tmp_folder):
-    os.makedirs(tmp_folder)
+if not os.path.exists(out_folder):
+    os.makedirs(out_folder)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((lhost, lport))
+sock.bind((self_host, self_port))
 sock.listen(5)
 while True:
     client, addr = sock.accept()
